2c625b6d3b8e4e469344800a7dd9f744a262bdfa
vmukti <81781432+vmukti@users.noreply.github.com>
2026-02-22 12:50:45 +0530

    seo: import all valid SEO data files (Compare, Compliance, Expansion2, GenAI) into landing page

--- src/pages/SEOLandingPages/SEOLandingPage.jsx
+++ src/pages/SEOLandingPages/SEOLandingPage.jsx
@@ -26,6 +26,10 @@ import { CheckCircleIcon, ChevronRightIcon } from '@chakra-ui/icons';
 import seoPageData from '../../data/seoPageData';
 import seoPageDataExpansion from '../../data/seoPageDataExpansion';
 import seoPageDataInnovation from '../../data/seoPageDataInnovation';
+import seoPageDataCompare from '../../data/seoPageDataCompare';
+import seoPageDataCompliance from '../../data/seoPageDataCompliance';
+import seoPageDataExpansion2 from '../../data/seoPageDataExpansion2';
+import seoPageDataGenAI from '../../data/seoPageDataGenAI';
 
 
 // Stat Card Component
@@ -292,7 +296,7 @@ const SEOLandingPage = () => {
   const { category, pageSlug, name } = useParams();
   // When rendered from IndustryDetails fallback, route param is 'name' not 'pageSlug'
   const effectiveSlug = pageSlug || name;
-  const allSeoData = { ...seoPageData, ...seoPageDataExpansion, ...seoPageDataInnovation };
+  const allSeoData = { ...seoPageData, ...seoPageDataExpansion, ...seoPageDataInnovation, ...seoPageDataCompare, ...seoPageDataCompliance, ...seoPageDataExpansion2, ...seoPageDataGenAI };
   const lookupKey = (category && effectiveSlug && allSeoData[`${category}-${effectiveSlug}`]) ? `${category}-${effectiveSlug}` : (effectiveSlug && allSeoData[effectiveSlug]) ? effectiveSlug : (category && allSeoData[category]) ? category : null;
   const pageData = allSeoData[lookupKey];
 
